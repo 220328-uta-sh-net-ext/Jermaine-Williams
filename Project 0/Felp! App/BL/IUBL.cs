48d6a66b099eef13b4977a56a5ce62b788539a14
Jay Williams <j.williams2014@hotmail.com>
2022-05-16 14:19:43 -0400

    Remove duplicated directory

--- Project 0/Felp! App/BL/IUBL.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BL;
-
-public interface IUBL // business logic interface for Users
-{
-    User AddUser(User newuser); // method that will add a new user to the list
-
-    List<User> GetAllUsers();// list of all the users that are already added to the repo
-}
-
